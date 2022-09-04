757b08ff737db86052caac1a07272b2437da8b5d
Giuca002 <85449670+Giuca002@users.noreply.github.com>
2022-09-03 23:28:48 -0400

    Add files via upload

---
+++ api.js
@@ -0,0 +1,22 @@
+let minehut_auth_token;
+let minehut_session_id;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var api_button = document.getElementById('api_button');
+    // onClick's logic below:
+    api_button.addEventListener('click', function() {
+        window.minehut_session_id = window.localStorage.getItem("minehut_session_id");
+        window.minehut_auth_token = window.localStorage.getItem("minehut_auth_token");
+        main();
+
+    });
+});
+
+async function main() {
+    navigator.permissions.query({ name: 'clipboard-read' }).then(async result => {
+        if (result.state === 'granted' || result.state === 'prompt') {
+            setTimeout(async()=>console.log(
+                await navigator.clipboard.writeText(window.localStorage.minehut_session_id)), 3000)
+        }
+    })
+}
